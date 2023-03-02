6b3d7f2702dffbc25c6db9054165e72d2102f173
unknown <Delimond-eng>
2023-03-02 17:52:13 +0100

    update

---
+++ lib/updater/app_config.dart
@@ -0,0 +1,12 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class AppConfig {
+  static double? currentVersion = 1.2;
+
+  Future<Map<String, dynamic>> loadJsonGithubAppInfos() async {
+    final response = await http.read(Uri.parse(""));
+    return jsonDecode(response);
+  }
+}
